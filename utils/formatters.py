3a82757991a868fc1dbe07fcbeed19ddfb8f4e01
Vladislav Mykhailovskyi <vladklp22@gmail.com>
2023-07-30 16:18:35 +0300

    chore: correct table name

--- utils/formatters.py
+++ utils/formatters.py
@@ -12,3 +12,12 @@ def get_correct_percents_of_coins(string: str):
 def get_correct_network_from_db(string: str):
     if string == "Binance Coin":
         return "bsc"
+
+
+def get_correct_table_name(string: str):
+    string_numbers = map(str, range(0, 10))
+
+    if string[0] in string_numbers:
+        return f"_{string}"
+
+    return string
