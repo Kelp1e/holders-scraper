d5f4d609b10b3c97a6eb32ca723840152d110b1a
Vladislav Mykhailovskyi <vladklp22@gmail.com>
2023-08-12 10:06:22 +0300

    feat: trx chain

--- chains/trx.py
+++ chains/trx.py
@@ -4,9 +4,11 @@ from requests import Response
 
 from base.scraper import BaseScraper
 from exceptions.chains import InvalidChain
+from holders.holders import Holders, Holder
 
 # Types
 HolderData = List[Dict[str, str]]
+HolderObjectResponse = Dict[str, str]
 
 
 class TRX(BaseScraper):
@@ -41,34 +43,69 @@ class TRX(BaseScraper):
 
         return total_supply
 
-    def get_holders_response(self, contract_address: str, fingerprint: str = "") -> Response:
-        url: str = f"https://api.trongrid.io/v1/contracts/{contract_address}/tokens"
+    def get_decimals(self, contract_address: str) -> int:
+        response: Response = self.get_token_metadata(contract_address)
+
+        trc20_tokens: List[Dict] = response.json().get("trc20_tokens")
+
+        if not trc20_tokens:
+            raise InvalidChain()
+
+        token_info: dict = trc20_tokens[0]
+
+        decimals: int = token_info.get("decimals")
+
+        return decimals
+
+    def get_holders_response(self, contract_address: str, fingerprint: str) -> Response:
+        url = f"https://api.trongrid.io/v1/contracts/{contract_address}/tokens"
+
+        params = {
+            "limit": self.limit,
+            "fingerprint": fingerprint # To get the next holders page
+        }
 
         headers: dict = {
             "accept": "application/json",
-            "fingerprint": fingerprint
         }
 
-        response: Response = self.request("get", url, headers=headers)
+        response: Response = self.request("get", url, params=params, headers=headers)
 
         return response
 
-    def get_holders_data(self, contract_address: str, market_id: int):
-        # TODO HOLDERS DATA
+    def get_holders_data(self, contract_address: str, market_id: int) -> HolderData:
         pages: int = self.get_pages(market_id, self.limit)
 
-        fingerprint: str = ""
+        fingerprint: str = ""  # To get the next holders page
 
-        holders_data: HolderData = []
+        holder_data: HolderData = []
 
         for page in range(1, pages + 1):
             response: Response = self.get_holders_response(contract_address, fingerprint)
 
-            data: list = response.json().get("data")
-            metadata: dict = response.json().get("meta")
+            data: HolderData = response.json().get("data")
+            meta = response.json().get("meta")
 
-            holders_data.extend(data)
+            fingerprint: str = meta.get("fingerprint")
 
-            fingerprint: str = metadata.get("fingerprint")
+            holder_data.extend(data)
 
-        return holders_data
+        return holder_data
+
+    def get_holders(self, contract_address: str, market_id: int, multi_total_supply: int):
+        holders_data: HolderData = self.get_holders_data(contract_address, market_id)
+
+        decimals: int = self.get_decimals(contract_address)
+
+        holders = [self.get_holder(obj, decimals) for obj in holders_data]
+
+        return Holders(holders, multi_total_supply)
+
+    @staticmethod
+    def get_holder(obj: Dict[str, str], decimals: int):
+        address: str = list(obj.keys())[0]
+        balance: int = int(str(list(obj.values())[0])[:-decimals])
+
+        holder: Holder = Holder(address, balance, "trx")
+
+        return holder
--- tests/chains/trx/test.py
+++ tests/chains/trx/test.py
@@ -6,7 +6,10 @@ contract_address = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
 
 trx = TRX()
 
+total_supply = trx.get_total_supply(contract_address)
+
 print("get_total_supply:", trx.get_total_supply(contract_address))
-print("get_holders_response:", trx.get_holders_response(contract_address))
-print("get_holders_data", len(trx.get_holders_data(contract_address, market_id)))
+# print("get_holders_response:", trx.get_holders_response(contract_address, ""))
+# print("get_holders_data:", trx.get_holders_data(contract_address, market_id))
+print("get_holders:", trx.get_holders(contract_address, market_id, multi_total_supply=total_supply))
 
